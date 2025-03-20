73235126c45c42457ca97b8f452586f95a604657
Norm Cervantes <normancervantes701@gmail.com>
2025-03-20 01:19:47 -0500

    Fixed and debugged Module2Discussion.cpp

--- Module2Discussion.cpp
+++ Module2Discussion.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
     int age;
     char initial;
-    
-    cout << "Enter your age: "
+
+    cout << "Enter your age: ";  // ✅ FIXED: Added semicolon
     cin >> age;
+
     cout << "Enter the first letter of your last name: ";
-    cin << initial;
-    
+    cin >> initial;  // ✅ FIXED: Changed '<<' to '>>'
+
     cout << "You are " << age << " years old and your last name starts with " << initial << endl;
-    
+
     return 0;
 }
